78afd4cc00e0c5ae4900e4a6ac552e933e9de3fa
Sumanth Naik <sumanthmuttu524@gmail.com>
2022-07-07 19:48:10 +0530

    first app

---
+++ sumsapp/app/src/main/java/com/example/sumsapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sumsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void Login(View view ){
+        Toast.makeText(this, "entering please wait...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
